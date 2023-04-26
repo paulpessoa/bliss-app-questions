355d79e3c2dbb244dd46e0ba88cf3cf815c74a5d
Paul Pessoa <paulmspessoa@gmail.com>
2023-04-26 13:27:58 -0300

    setuping _app.tsx, with a very simple Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,35 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import ConnectionStatus from '../../components/ConnectionStatus';
+import Link from 'next/link'
+interface LayoutProps {
+  children: React.ReactNode;
 }
+
+const Layout = ({ children }: LayoutProps) => {
+
+  return (
+    <div >
+      <header>
+        <h2> Bliss App Questions </h2>
+      </header>
+      <main>
+        {children}
+      </main>
+      <footer>
+        <Link href="https://www.linkedin.com/in/paulmspessoa/" target='_blank'>Paul Pessoa</Link>
+        <ConnectionStatus />
+      </footer>
+    </div>
+  );
+};
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
+
+export default App;
