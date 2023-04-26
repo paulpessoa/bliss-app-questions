cfc357b40f0087a4b6fdf4a144a5261c3bd51559
Paul Pessoa <paulmspessoa@gmail.com>
2023-04-26 13:26:53 -0300

    check conection status with navigator.onLine

---
+++ components/ConnectionStatus.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+const ConnectionStatus = (): JSX.Element => {
+  const [isOnline, setIsOnline] = useState<boolean>(false);
+  const [showNotification, setShowNotification] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleOnline = () => {
+      setIsOnline(true);
+      setShowNotification(true);
+    };
+
+    const handleOffline = () => {
+      setIsOnline(false);
+      setShowNotification(true);
+    };
+
+    if (typeof window !== 'undefined') {
+      setIsOnline(window.navigator.onLine);
+
+      window.addEventListener("online", handleOnline);
+      window.addEventListener("offline", handleOffline);
+
+      return () => {
+        window.removeEventListener("online", handleOnline);
+        window.removeEventListener("offline", handleOffline);
+      };
+    }
+  }, []);
+
+  const handleCloseNotification = () => {
+    setShowNotification(false);
+  };
+
+  return (
+    <>
+      {showNotification && (
+        <div className={`notification ${isOnline ? "success" : "error"}`}>
+          <span className="text">{isOnline ? 'You are connected!' : 'You are offline!'}</span>
+          <button onClick={handleCloseNotification} className="close">X</button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ConnectionStatus;
