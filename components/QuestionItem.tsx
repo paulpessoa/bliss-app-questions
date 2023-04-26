31f3123da13832b05f367ff6f4fc5c28dae3cc4b
Paul Pessoa <paulmspessoa@gmail.com>
2023-04-26 13:32:00 -0300

    start questionItem component

---
+++ components/QuestionItem.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+type Question = {
+  id: number;
+  question: string;
+  image_url: string;
+  thumb_url: string;
+  published_at: string;
+  choices: {
+    choice: string;
+    votes: number;
+  }[];
+};
+
+type QuestionItemProps = {
+  questions: Question[];
+};
+
+const QuestionItem = ({ questions }: QuestionItemProps) => {
+  const router = useRouter();
+
+  const handleQuestionClick = (id: number) => {
+    router.push(`/questions/${id}`);
+  };
+
+  return (
+    <div className="question-box">
+      {questions.map((question) => (
+        <div key={question.id} className="content" onClick={() => handleQuestionClick(question.id)}>
+            <div className="title">
+              <h2>{question.id}. {question.question}</h2>
+            </div>
+            <div className="image">
+              <Image src={question.thumb_url} alt={question.question} width={120} height={120} />
+            </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default QuestionItem;
